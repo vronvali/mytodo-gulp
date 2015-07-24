3fc33278da89a6533d0666c8b964f1948dc052bd
Travis <vronvali@cesine.ca>
2015-07-24 12:51:34 -0400

    Changing files from tutorific version to word cloud version

---
+++ src/app/tutorial/tutorial.directive.js
@@ -0,0 +1,118 @@
+"use strict";
+/* globals document, window, console */
+
+/**
+ * @ngdoc directive
+ * @name wordCloudChrome.directive:fielddbTutorial
+ * @description
+ * # fielddbTutorial
+ */
+angular.module("wordCloudChrome").directive("tutorialSlider", function() {
+  return {
+    templateUrl: "app/components/tutorial/tutorial.html",
+    restrict: "A",
+    transclude: false,
+    scope: {
+      tutorial: "=json"
+    },
+    controller: function($scope) {
+    $scope.tutorialpopups = ["tutorialExplanationSlide", "tutorialBeginSlide", "tutorialEditSlide", "tutorialFinishSlide"];
+    var currentPopUp = "";
+
+    function toggle(div_id) {
+      var el = document.getElementById(div_id);
+      if (!el){
+          console.warn('unable to find '+div_id+' on the page are you sure you have an element with id '+div_id);
+          return;
+        }
+      if (el.style.display === 'none') {
+        el.style.display = 'block';
+      } else {
+        el.style.display = 'none';
+      }
+    }
+
+    function blanket_size() {
+       var viewportheight;
+        var blanket_height;
+        var popUpDiv_height;
+      if (typeof window.innerWidth !== 'undefined') {
+        viewportheight = window.innerHeight;
+      } else {
+        viewportheight = document.documentElement.clientHeight;
+      }
+      if ((viewportheight > document.body.parentNode.scrollHeight) && (viewportheight > document.body.parentNode.clientHeight)) {
+        blanket_height = viewportheight;
+      } else {
+        if (document.body.parentNode.clientHeight > document.body.parentNode.scrollHeight) {
+          blanket_height = document.body.parentNode.clientHeight;
+        } else {
+          blanket_height = document.body.parentNode.scrollHeight;
+        }
+      }
+      var blanket = document.getElementById('blanket');
+      if (!blanket){
+          console.warn('unable to find blanket on the page are you sure you have an element with id blanket');
+          return;
+        }
+      blanket.style.height = blanket_height + 'px';
+      for (var i = tutorialpopups.length - 1; i >= 0; i--) {
+        var popUpDiv = document.getElementById(tutorialpopups[i]);
+        popUpDiv_height = blanket_height / 2 - 200; //200 is half popup's height
+        popUpDiv.style.top = popUpDiv_height + 'px';
+      }
+    }
+
+    function window_pos(popUpDivVar) {
+      var viewportwidth;
+        var window_width;
+      if (typeof window.innerWidth !== 'undefined') {
+        viewportwidth = window.innerHeight;
+      } else {
+        viewportwidth = document.documentElement.clientHeight;
+      }
+      if ((viewportwidth > document.body.parentNode.scrollWidth) && (viewportwidth > document.body.parentNode.clientWidth)) {
+        window_width = viewportwidth;
+      } else {
+        if (document.body.parentNode.clientWidth > document.body.parentNode.scrollWidth) {
+          window_width = document.body.parentNode.clientWidth;
+        } else {
+          window_width = document.body.parentNode.scrollWidth;
+        }
+      }
+      var popUpDiv = document.getElementById(popUpDivVar);
+      if (!popUpDiv){
+          console.warn('unable to find popUpDiv on the page are you sure you have an element with id popUpDiv');
+          return;
+        }
+      window_width = window_width / 2 - 200; //200 is half popup's width
+      popUpDiv.style.left = window_width + 'px';
+    }
+
+    function popup(windowname) {
+      blanket_size(windowname);
+      window_pos(windowname);
+      toggle('blanket');
+      toggle(windowname);
+    }
+
+    $scope.next_popup= function(popups) {
+      if (currentPopUp) {
+        toggle(currentPopUp);
+      }
+      if (!popups || popups.length < 1) {
+        toggle("blanket");
+        return;
+      }
+      var popupname = popups.splice(0, 1);
+      popup(popupname);
+      currentPopUp = popupname;
+
+      // body...
+    };
+    //This will cause the popup to show immediently
+    $scope.next_popup($scope.tutorialpopups);
+  },
+  link: function postlink() {}
+};
+});
---
+++ src/app/tutorial/tutorial.directive.spec.js
@@ -0,0 +1,109 @@
+"use strict";
+/* globals  console */
+var debugMode = false;
+
+describe("Directive: tutorial", function() {
+
+  // load the directive's module and the template
+  beforeEach(module("wordCloudChrome"));
+  beforeEach(module("wordCloudChromeTemplates"));
+  var el, scope, compileFunction;
+
+  beforeEach(inject(function($rootScope, $compile) {
+    el = angular.element("<div data-tutorial-slider json='tutorial2'></div> <div data-tutorial-slider json='tutorial1'></div>");
+    scope = $rootScope.$new();
+    scope.tutorial1 = ["tutorialExplanationSlide", "tutorialBeginSlide", "tutorialEditSlide", "tutorialFinishSlide"];
+    scope.tutorial2 = ["tutorialExplanationSlide", "tutorialBeginSlide", "tutorialEditSlide", "tutorialFinishSlide"];
+    compileFunction = $compile(el);
+    // bring html from templateCache
+    scope.$digest();
+    if (debugMode) {
+      console.log("post compile", el.html()); // <== html here has {{}}
+    }
+  }));
+
+  // http://stackoverflow.com/questions/17223850/how-to-test-directives-that-use-templateurl-and-controllers
+  it("should make a tutorial element with contents from scope", function() {
+
+    inject(function() {
+      compileFunction(scope); // <== the html {{}} are bound
+      try {
+        if (!scope.$$phase) {
+          scope.$digest(); // <== digest to get the render to show the bound values
+        }
+      } catch (e) {
+        console.log("error digesting scope");
+      }
+      if (debugMode) {
+        console.log("post link", el.html());
+        console.log("scope tutorial2 ", scope.tutorial2);
+        console.log("scope tutorial1 ", scope.tutorial1);
+      }
+      //expect(angular.element(el).text().trim()).toEqual("hello hello");
+      expect(angular.element(el.find("h1")).text().trim()).toEqual('Welcome to iLanguageCloud!');
+      expect(angular.element(el.find("h2")).text().trim()).toEqual('Begin');
+      expect(angular.element(el.find("h3")).text().trim()).toEqual('Edit');
+      expect(angular.element(el.find("h4")).text().trim()).toEqual('Finished?');
+    });
+  });
+  xit("should have an array of slides", function() {
+
+    inject(function() {
+      compileFunction(scope); // <== the html {{}} are bound
+      try {
+        if (!scope.$$phase) {
+          scope.$digest(); // <== digest to get the render to show the bound values
+        }
+      } catch (e) {
+        console.log("error digesting scope");
+      }
+      if (debugMode) {
+        console.log("post link", el.html());
+        console.log("scope tutorial2 ", scope.tutorial2);
+        console.log("scope tutorial1 ", scope.tutorial1);
+      }
+      expect(angular.element(el).text().trim()).toEqual("hello");
+      // expect(angular.element(el.find("dd")[0]).text().trim()).toEqual("hello");
+    });
+  });
+  xit("should start on the first slide", function() {
+
+    inject(function() {
+      compileFunction(scope); // <== the html {{}} are bound
+      try {
+        if (!scope.$$phase) {
+          scope.$digest(); // <== digest to get the render to show the bound values
+        }
+      } catch (e) {
+        console.log("error digesting scope");
+      }
+      if (debugMode) {
+        console.log("post link", el.html());
+        console.log("scope tutorial2 ", scope.tutorial2);
+        console.log("scope tutorial1 ", scope.tutorial1);
+      }
+      expect(angular.element(el).text().trim()).toEqual("hello");
+      // expect(angular.element(el.find("dd")[0]).text().trim()).toEqual("hello");
+    });
+  });
+  xit("should go to the next slide if user pushes next", function() {
+
+    inject(function() {
+      compileFunction(scope); // <== the html {{}} are bound
+      try {
+        if (!scope.$$phase) {
+          scope.$digest(); // <== digest to get the render to show the bound values
+        }
+      } catch (e) {
+        console.log("error digesting scope");
+      }
+      if (debugMode) {
+        console.log("post link", el.html());
+        console.log("scope tutorial2 ", scope.tutorial2);
+        console.log("scope tutorial1 ", scope.tutorial1);
+      }
+      expect(angular.element(el).text().trim()).toEqual("hello");
+      // expect(angular.element(el.find("dd")[0]).text().trim()).toEqual("hello");
+    });
+  });
+});
